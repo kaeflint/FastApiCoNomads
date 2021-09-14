569beb91fc064312c9370fcab647459cde830bd8
kaeflint <esseljojo1990@gmail.com>
2021-09-14 02:05:32 +0100

    SCTP4

--- utils/config.py
+++ utils/config.py
@@ -2,3 +2,6 @@ ADMIN_NAME = 'manager'
 ADMIN_PASSWORD = "manager_pass419"
 
 DATABASE = "application_db"
+DATABASE_SERVER = "mysql+mysqlconnector://admin:Melody2Jason@database-1.csptufuwdfrf.us-west-2.rds.amazonaws.com:3306"
+
+dummy_access_copy_heroku_connector = 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
